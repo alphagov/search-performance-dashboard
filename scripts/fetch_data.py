f223f355f42ab5608f6b090055c708d8984c8d00
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-03-17 12:54:17 +0000

    Add command line option to start fetch a given number of days ago

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -8,15 +8,25 @@ import sys
 
 logging.basicConfig(level=logging.INFO)
 
-def main(max_days_ago):
-    for days_ago in range(1, max_days_ago + 1):
+
+def main(min_days_ago, max_days_ago):
+    for days_ago in range(min_days_ago, max_days_ago + 1):
+        print "Fetching data for %d days ago" % (days_ago, )
         date = datetime.date.today() - datetime.timedelta(days=days_ago)
         index_daily_data(date)
+
 
 if __name__ == '__main__':
     # Yesterday's data is usually sampled; the day before is usually now
     # available unsampled.  Fetch the previous day's data.
+    min_days_ago = 1
     max_days_ago = 2
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         max_days_ago = int(sys.argv[1])
-    main(max_days_ago)
+    elif len(sys.argv) == 3:
+        min_days_ago = int(sys.argv[1])
+        max_days_ago = int(sys.argv[2])
+    elif len(sys.argv) != 1:
+        print "Usage: %s [[min_days_ago] max_days_ago]" % (sys.argv[0],)
+        sys.exit(1)
+    main(min_days_ago, max_days_ago)
