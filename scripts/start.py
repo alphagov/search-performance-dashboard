91176eb9937521ff07ba406e249b5f8f2436ebd8
Richard Boulton <richard.boulton@digital.cabinet-office.gov.uk>
2014-03-17 08:13:56 +0000

    Serve only to localhost for now

--- scripts/start.py
+++ scripts/start.py
@@ -1,8 +1,11 @@
-from dashboard.serve import app
+import os
 import sys
+up = os.path.dirname
+sys.path.insert(0, up(up(os.path.abspath(__file__))))
 
 
 if __name__ == '__main__':
+    from dashboard.serve import app
     if '--debug' in sys.argv[1:]:
         app.debug = True
-    app.run(host='0.0.0.0', port=4001)
+    app.run(host='127.0.0.1', port=4001)
